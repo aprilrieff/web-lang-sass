994bd0d48fd8966ddeff7f17d75be62709e4dd37
April Rieff <april.rieff@gmail.com>
2022-11-29 22:11:09 -0600

    advice generator

--- dist/advice/advice.js
+++ dist/advice/advice.js
@@ -1,16 +1,34 @@
-const advice = document.getElementById("advice-container");
+const advice = document.querySelector(".advice-container h2");
 const btn = document.getElementById("diceBtn");
 const adviceNum = document.getElementById("advice-number");
+const numArray = [];
 
-function getAdvice() {
-    fetch("https://api.adviceslip.com/advice")
-        .then((response) => response.json())
-        .then((data) => console.log(data));
+async function getAdvice() {
+    const url = `https://api.adviceslip.com/advice/{slip_id}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
 
-    advice.innerHTML = `<p>${data.advice}</p>`;
+    adviceNum.innerHTML = `ADVICE #${data.slip.id}`;
+    advice.innerHTML = `${data.slip.advice}`;
+
+
+}
+
+function getRandomSlip (min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function storeNum(){
+    if(numArray.includes(adviceNum)){
+        getRandomSlip();
+    } else {
+        numArray.push(adviceNum);
+    }
 }
 
 
+getRandomSlip(1, 225);
 getAdvice();
+
 btn.addEventListener("click", getAdvice);
-adviceNum.addEventListener("click", adviceNum.innerHTML(Math.random()))
